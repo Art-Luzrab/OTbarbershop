4b31f30992a2d8768b184735f11bf90ccdf9188a
Arthur <arthur91021@gmail.com>
2025-08-15 01:04:35 -0700

    created book now link, routing is next

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,15 @@ import logo from "../assets/logo.jpg";
 
 function Navbar() {
   return (
-    <nav className="border-b-2 bg-[black]">
-      <img src={logo} className="ml-2 h-20 w-40" />
+    <nav className="flex h-20 items-center justify-between border-b-2 bg-black text-white">
+      <img src={logo} className="ml-2 max-h-full w-auto object-contain" />
+
+      <a
+        href="/book"
+        className="font-anton flex h-full items-center px-6 text-xl tracking-wider transition-colors duration-700 ease-in-out hover:bg-white hover:text-black"
+      >
+        BOOK NOW
+      </a>
     </nav>
   );
 }
