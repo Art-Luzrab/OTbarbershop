e49d861b220e6e040be73bfe6e2f99d00485c6fb
Arthur <arthur91021@gmail.com>
2025-08-19 16:36:15 -0700

    fixed nav height so that book now button hightlights white vertically fully

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,16 +3,16 @@ import logo from "../assets/logo.jpg";
 
 function Navbar() {
   return (
-    <nav className="h-21 flex items-center justify-between border-b-2 bg-black text-white">
+    <nav className="flex h-[82px] items-center justify-between border-b-2 bg-black text-white">
       <Link to="/" className="flex h-full items-center">
         <img src={logo} alt="Logo" className="ml-2 h-20 w-40 cursor-pointer" />
       </Link>
-      <a
-        href="/book"
+      <Link
+        to="/book"
         className="flex h-full items-center px-6 font-anton text-xl tracking-wider transition-colors duration-700 ease-in-out hover:bg-white hover:text-black"
       >
         BOOK NOW
-      </a>
+      </Link>
     </nav>
   );
 }
