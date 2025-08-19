3f6e4eddae7180edd63e32e9e583b9b274a74f63
Arthur <arthur91021@gmail.com>
2025-08-19 15:57:19 -0700

    added basic routes to two pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import HomePage from "./components/HomePage";
+import BookingPage from "./components/BookingPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/booking",
+    element: <BookingPage />,
+  },
+]);
 
 function App() {
   return (
     <>
       <Navbar />
+      <RouterProvider router={router} />
     </>
   );
 }
---
+++ src/components/HomePage.jsx
@@ -0,0 +1,5 @@
+function HomePage() {
+  return <div className="text-yellow-500">This is the Home Intro Page!</div>;
+}
+
+export default HomePage;
