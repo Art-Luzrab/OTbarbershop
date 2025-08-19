2d42861bb7a4da2c84aa4b61a92252f553b2792a
Arthur <arthur91021@gmail.com>
2025-08-19 16:21:29 -0700

    logo links to home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,16 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import HomePage from "./components/HomePage";
 import BookingPage from "./components/BookingPage";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomePage />,
-  },
-  {
-    path: "/booking",
-    element: <BookingPage />,
-  },
-]);
 
 function App() {
   return (
     <>
       <Navbar />
-      <RouterProvider router={router} />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/book" element={<BookingPage />} />
+      </Routes>
     </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,15 @@
+import { Link } from "react-router-dom";
 import logo from "../assets/logo.jpg";
 
 function Navbar() {
   return (
-    <nav className="flex h-20 items-center justify-between border-b-2 bg-black text-white">
-      <img src={logo} className="ml-2 max-h-full w-auto object-contain" />
-
+    <nav className="h-21 flex items-center justify-between border-b-2 bg-black text-white">
+      <Link to="/" className="flex h-full items-center">
+        <img src={logo} alt="Logo" className="ml-2 h-20 w-40 cursor-pointer" />
+      </Link>
       <a
         href="/book"
-        className="font-anton flex h-full items-center px-6 text-xl tracking-wider transition-colors duration-700 ease-in-out hover:bg-white hover:text-black"
+        className="flex h-full items-center px-6 font-anton text-xl tracking-wider transition-colors duration-700 ease-in-out hover:bg-white hover:text-black"
       >
         BOOK NOW
       </a>
