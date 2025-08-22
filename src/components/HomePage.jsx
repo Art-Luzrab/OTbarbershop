aeb62c8d2252b7790cae42849fd8f197e4e82ffe
Arthur <arthur91021@gmail.com>
2025-08-22 01:54:30 -0700

    img is now used as background

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,11 +1,12 @@
-import barbershop from "../assets/barbershop.jpg";
+import barbershop from "../assets/largebarber.jpg";
 
 function HomePage() {
   return (
     <>
-      <div className="flex-col justify-items-center">
-        <img src={barbershop} alt="barbershop" className="" />
-      </div>
+      <div
+        className="h-screen w-screen bg-cover bg-center"
+        style={{ backgroundImage: `url(${barbershop})` }}
+      ></div>
     </>
   );
 }
