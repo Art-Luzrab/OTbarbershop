8d698f85840bb5872211e8d94d3b66895952bac8
Arthur <arthur91021@gmail.com>
2025-08-22 02:30:15 -0700

    basic intro page, still need some tweaking

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -4,9 +4,23 @@ function HomePage() {
   return (
     <>
       <div
-        className="h-screen w-screen bg-cover bg-center"
+        className="relative h-screen w-screen bg-cover bg-center"
         style={{ backgroundImage: `url(${barbershop})` }}
-      ></div>
+      >
+        {/* Overlay  */}
+        <div className="absolute inset-0 bg-black/65"></div>
+
+        {/* Content */}
+        <div className="relative flex h-full flex-col items-center justify-center">
+          <h1 className="relative bg-black/85 p-5 text-center font-anton text-4xl text-white drop-shadow-lg">
+            Welcome to OTBarbershop
+          </h1>
+          <p className="mr-2 mt-6 text-center text-lg font-bold italic text-white drop-shadow-xl">
+            A modern barbershop experience where classic cuts meet clean style.
+            Walk in confident, walk out sharper.
+          </p>
+        </div>
+      </div>
     </>
   );
 }
